83fc5c4c98a110542f0ead7e078afbe966e1e74f
Devashnee <devashneebhagawandin@hotmail.com>
2016-03-13 15:48:57 +0200

    functions created

--- tut4-TicTacToe/TicTacToe.h
+++ tut4-TicTacToe/TicTacToe.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+#ifndef TicTacToe_H
+#define TicTacToe_H
+
 class TicTacToe
 {
+private:
+	char game[3][3];    //declearing a 3x3 array
+	bool shift(int, int);
+
 public:
 	TicTacToe();
-	~TicTacToe();
+	void Instructions();
+	void print();    
+	void move1(int);
+	void move2(int);
+	bool over();
+	bool Win();
+	bool Draw();
+	void reset();
 };
 
+#endif
