937dcd66394c25c71341e2d021db0f37f98af33e
Devashnee <devashneebhagawandin@hotmail.com>
2016-03-13 15:51:10 +0200

    constructor initailised

--- tut4-TicTacToe/TicTacToe.cpp
+++ tut4-TicTacToe/TicTacToe.cpp
@@ -1,8 +1,12 @@
 #include "TicTacToe.h"
 
 
-TicTacToe::TicTacToe()
+TicTacToe::TicTacToe()//default constructor initailised
 {
+
+	game[0][0] = '1'; game[0][1] = '2'; game[0][2] = '3';
+	game[1][0] = '4'; game[1][1] = '5'; game[1][2] = '6';
+	game[2][0] = '7'; game[2][1] = '8'; game[2][2] = '9';
 }
 
 
